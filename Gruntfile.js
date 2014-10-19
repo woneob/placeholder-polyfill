54e23852979a0512d2a567f126bd7639e05dcf48
Cho WonEob <1up@1upz.com>
2014-10-20 03:37:19 +0900

    placeholder polyfill 등록

---
+++ Gruntfile.js
@@ -0,0 +1,64 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		banner: [
+			'/*!',
+			' * Name: <%= pkg.name %> - <%= pkg.description %>',
+			' * Version: v<%= pkg.version %>',
+			' * Created: <%= grunt.template.today("yyyy-mm-dd") %>',
+			' * Homepage: <%= pkg.homepage %>',
+			' * License: <%= pkg.license %>',
+			' */\n\n'
+		].join('\n'),
+		jshint: {
+			hint: {
+				files: {
+					src: 'src/**/*.js'
+				}
+			}
+		},
+		clean: {
+			options: {
+				force: true
+			},
+			dist: 'dist/**/*.*'
+		},
+		uglify: {
+			beautify: {
+				options: {
+					banner: '<%= banner %>',
+					mangle: false,
+					compress: false,
+					beautify: true,
+					preserveComments: true,
+					footer: '\n'
+				},
+				files: [{
+					expand: true,
+					cwd: 'src/',
+					src: ['**/*.js', '!**/*.min.js'],
+					dest: 'dist/'
+				}]
+			},
+			minify: {
+				options: {
+					banner: '<%= banner %>',
+					footer: '\n'
+				},
+				files: [{
+					expand: true,
+					cwd: 'src/',
+					src: ['**/*.js', '!**/*.min.js'],
+					dest: 'dist/',
+					ext: '.min.js',
+					extDot: 'last'
+				}]
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.registerTask('default', ['jshint', 'clean', 'uglify']);
+};
